fe205c88786d0bdf8220338477348900ccb87399
Serkan Koç <serkan.koc@marun.edu.tr>
2022-10-26 15:39:13 +0300

    Finished Section 8: Spring Configuration with Java Annotations - Dependency Injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,42 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach{
+    @Autowired
+    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
+
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+    // define a default constructor
+
+
+    public TennisCoach() {
+        System.out.println(">> Tennish Coach: inside default constructor");
+
+    }
+
+    // define a setter method
+//    @Autowired
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennishCoach: inside doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
